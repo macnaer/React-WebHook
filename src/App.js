91854736903dc7e6f6f0a533c7d9c1f2b02aa598
master <master@rivne.itstep.org>
2021-03-08 14:21:37 +0200

    Start useState

--- src/App.js
+++ src/App.js
@@ -1,35 +1,60 @@
-import React from "react";
+import React, {useState} from "react";
 import { v4 as uuid } from 'uuid';
 import './App.css';
 
 // ToDoList
 import ToDoList from "./Components/ToDoList/ToDoList";
 
-class App extends React.Component {
+const App = () => {
 
-  state = {
-    todos: 
-    [
-      {id: uuid, title: "Learn react", completed: false },
-      {id: uuid, title: "Learn english", completed: false },
-      {id: uuid, title: "infotime", completed: false },
-    ]
+  // state = {
+  //   todos: 
+  //   [
+  //     {id: uuid, title: "Learn react", completed: false },
+  //     {id: uuid, title: "Learn english", completed: false },
+  //     {id: uuid, title: "infotime", completed: false },
+  //   ]
   
-  }
+  // }
 
-  render(){
-    const { todos } = this.state;
+    // const [todos, setTodos] = useState([
+    //   {id: uuid, title: "Learn react", completed: false },
+    //    {id: uuid, title: "Learn english", completed: false },
+    //    {id: uuid, title: "infotime", completed: false },
+    // ])
+
+    const [todos, setTodos] = useState([]);
+    const [todoTitle, setTodoTitle] = useState("")
+
+    const addTodo = event => {
+      if (event.key === "Enter"){
+        setTodos([
+          ...todos,
+          {
+            id: uuid(),
+            title: todoTitle,
+            completed: false
+          }
+        ])
+        setTodoTitle("");
+      }
+    }
+
+    // const { todos } = this.state;
     return (
       <div className="container">
         <h1>Todo List</h1>
         <div className="form-group">
-          <input type="text"  />
+          <input type="text"
+            value={todoTitle}
+            onChange={event => setTodoTitle(event.target.value)}
+            onKeyPress={addTodo}
+          />
           <label>Add new todo</label>
         </div>
         <ToDoList todos={todos} />
       </div>
     );
-  }
 }
 
 export default App;
